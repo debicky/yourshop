0b9689c18328bdd56bff3eb79cdf8727bf596cc6
debicky <arturd444@gmail.com>
2020-04-13 18:39:12 +0200

    order and order item

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,21 @@
 class Product < ApplicationRecord
+  has_many :order_items
   belongs_to :category
   has_one_attached :img
+  before_destroy :ensure_noone_is_buying
+
+
 
   validates :title, :body, :img, :price, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
-  validates :img, allow_blank: true, format: {
-    with: %r{\.(jpeg|jpg|png)\z}i,
-    message: 'must be a URL for JPEG, JPG or PNG image.'}
+
+  private
+
+  def ensure_noone_is_buying
+    unless order_item.empty?
+      errors.add(:base, "Cart contains this item")
+      throw :abort
+    end
+  end
   
 end
