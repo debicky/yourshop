d643b7fe204ff6206f0b9343ae5a6c948c21f464
debicky <arturd444@gmail.com>
2020-04-22 16:13:29 +0200

    changed prodution rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.delivery_method = :smtp
   host = 'localhost:3000'
-  config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
+  config.action_mailer.default_url_options = { :host => "your-shop-debart.herokuapp.com" }
 
   config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
