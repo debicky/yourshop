f1aafb092904a7f2c2a67291f6b11187f5a2b3e1
debicky <arturd444@gmail.com>
2020-04-22 17:58:28 +0200

    fix7

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
 
   config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
-  port: 465,
+  port: 587,
   user_name: 'debartportfolio',
   password: 'dupasraka123!',
   authentication: 'plain',
